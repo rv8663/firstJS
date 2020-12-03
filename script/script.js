cb0446d9afcd263b1b852365213e4ceb63b76887
Vital <rv8663@gmail.com>
2020-12-03 17:27:02 +0300

    задача 1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
-const i=1;
-const b=1;
-console.log(i);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", 0);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("Насколько оцените", "");
+const c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("Насколько оцените", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
